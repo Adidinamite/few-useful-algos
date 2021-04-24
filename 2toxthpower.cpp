19b6d8203854ea7825c704fe38d0a7197abb0697
Adidinamite <61920198+Adidinamite@users.noreply.github.com>
2021-04-24 15:17:57 +0300

    Create 2toxthpower.cpp

---
+++ 2toxthpower.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    //the xth power of 2
+    //2 4 6 8 16 32 64 
+    int x; cin >> x;
+    cout << (64>>1);
+}

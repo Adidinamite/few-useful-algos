e9389699e4e30f333470aefc028439f5f721543b
Adidinamite <61920198+Adidinamite@users.noreply.github.com>
2021-04-29 19:00:34 +0300

    Update DEFAULT LIBRARY EXAMPLES.cpp

--- C++ TIPS & TRICKS-competitive_programming/SORTING ALGORITHMS/DEFAULT LIBRARY EXAMPLES.cpp
+++ C++ TIPS & TRICKS-competitive_programming/SORTING ALGORITHMS/DEFAULT LIBRARY EXAMPLES.cpp
@@ -12,8 +12,9 @@ int main()
 
     vector < int > v = {5, 6, 3, 5, 6, 2, 5, 7, 3};
     sort(v.begin(), v.end());
-    for( vector<int>::iterator it = v.begin(); it!=v.end(); it++)
-        cout<< *it<<" ";
+    int VecSize = v.size();
+    for(int i = 0; i<VecSize; i++)
+        cout << v[i] << " " ;
     cout<<"\n";
 
     int arr[] = {2,3,4,12,3,12,3,123,12,41,7};
@@ -22,6 +23,29 @@ int main()
     for(int i = 0; i<arrSize; ++i)
         cout<< arr[i]<<" ";
     cout<<"\n";
+
+    vector<pair<int, int>> perechi;
+    perechi.push_back({1, 5});
+    perechi.push_back({3, 1});
+    perechi.push_back({1, 3});
+    sort(perechi.begin(), perechi.end());
+    int sizeOfPairs = perechi.size();
+    for(int i = 0; i<sizeOfPairs; i++)
+        cout<< perechi[i].first <<" si "<< perechi[i].second<<" , ";
+    cout<<"\n";
+
+     
+    /*vector<tuple<int, int, int>> tuples;
+    tuples.push_back({2,1,4});
+    tuples.push_back({1,5,3});
+    tuples.push_back({2,1,3});
+    sort(tuples.begin(), tuples.end());
+    int sizeOfTuples = tuples.size();
+    for(int i = 0; i< sizeOfTuples; i++)
+    {
+        cout<< tuples[i].first<< " si "<< tuples[i].second << " si "<< tuples[i].third<< " , ";
+    }
+    cout<<"\n";*/
 
 }
 

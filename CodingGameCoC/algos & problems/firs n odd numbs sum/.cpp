e8cd9d01603edf1db5e4f5d18a4990af363d8c54
Adidinamite <61920198+Adidinamite@users.noreply.github.com>
2021-05-01 12:30:56 +0300

    Create .cpp

---
+++ CodingGameCoC/algos & problems/firs n odd numbs sum/.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n; 
+    cin >> n; cin.ignore();
+    cout<<(unsigned long long)(pow(n, 2));
+}

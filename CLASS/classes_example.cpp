bc059bf8472ad3d8ebee150b742eea2360558f3f
Adidinamite <61920198+Adidinamite@users.noreply.github.com>
2021-06-18 22:16:24 +0300

    Create classes_example.cpp

---
+++ CLASS/classes_example.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+    class Player
+{
+    public:
+        int x, y; // position
+        int speed; // speed
+    void Move(int xa, int ya)
+{
+    player.x += xa * player.speed;
+    player.y += ya * player.speed;
+}
+};
+
+/*void Move(Player& player, int xa, int ya)
+{
+    player.x += xa * player.speed;
+    player.y += ya * player.speed;
+}*/
+//Pot adauaga aceasta functie direct in clasa, numindu-se METODA fara sa mai fie nevoie de parametrul Player player
+int main()
+{
+    Player player;
+    player.x = 5;
+    player.Move(1, -1);
+    return 0;
+}

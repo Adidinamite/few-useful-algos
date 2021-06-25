c12489e6373379cbd54c2e422b203b8b359a5269
Adidinamite <61920198+Adidinamite@users.noreply.github.com>
2021-06-25 18:22:10 +0300

    Update mine_sweeper-StillInProgress.cpp

--- mine_sweeper-StillInProgress.cpp
+++ mine_sweeper-StillInProgress.cpp
@@ -2,13 +2,22 @@
 #include <cstring>
 #include <iomanip>
 using namespace std;
+#define EASY_SIZE 10
+#define MEDIUM_SIZE 17
+#define HARD_SIZE 25
 char difficulty[10];
 int choice;
 char difficulty_sure;
-bool player_decision = true;
+bool player_chooses = true;
+int table_size;
+    void refresh()
+    {
+        system("cls");
+    }
+
     void game_difficulty()
     {
-     while(player_decision){
+     while(player_chooses){
          cout<<setw(25)<<"CHOOSE YOUR DIFFICULTY:\n1.EASY\n2.MEDIUM\n3.HARD\n";
          cin >> choice;
         switch(choice)
@@ -27,13 +36,40 @@ bool player_decision = true;
         cout<<"y/n\n";
         cin >> difficulty_sure;
         if(difficulty_sure == 'y')
-            player_decision=false;
+            player_chooses=false;
+        else refresh();
     }
+    }
+    void create_matrix(char *difficulty)
+    {
+        if(difficulty == "EASY")
+        {
+            table_size = EASY_SIZE;
+            int playerBoard[EASY_SIZE][EASY_SIZE];
+            int realBoard[EASY_SIZE][EASY_SIZE];
+        }
+        if(difficulty == "MEDIUM")
+        {
+            table_size = MEDIUM_SIZE;
+            int playerBoard[MEDIUM_SIZE][MEDIUM_SIZE];
+            int realBoard[MEDIUM_SIZE][MEDIUM_SIZE];
+        }
+        if(difficulty == "HARD")
+        {
+            table_size = HARD_SIZE;
+            int playerBoard[HARD_SIZE][HARD_SIZE];
+            int realBoard[HARD_SIZE][HARD_SIZE];
+            
+        }
+    }
+    void Start()
+    {
+        create_matrix(difficulty);
     }
 int main()
 {
     game_difficulty();
-
-    cout<< difficulty;
+    refresh();
+    Start();
     return 0;
 }

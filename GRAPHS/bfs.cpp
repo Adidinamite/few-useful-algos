d92ed2562d024e842553dedd77920121dcb4bee6
Adidinamite <61920198+Adidinamite@users.noreply.github.com>
2021-12-22 17:35:01 +0200

    Update bfs.cpp

--- GRAPHS/bfs.cpp
+++ GRAPHS/bfs.cpp
@@ -3,8 +3,8 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-ifstream cin("dfs.in");
-ofstream cout("dfs.out");
+ifstream cin("BFS.in");
+ofstream cout("BFS.out");
 int n, m, x, p, q;
 vector <int >v[100002];
 int vizitat[100002];
@@ -15,30 +15,22 @@ void ascending(int max_nod)
         sort(v[k].begin(), v[k].end());
     }
 }
-void bfs(int s)
+void bfs(int nod)
 {
-    queue<int>q;
-    q.push(s);
-    vizitat[s] = 1;
-
-    cout<<s<<" ";
-
-    while(!q.empty())
-    {
-        for(int i=0;i<v[s].size();i++)
-        {
-            if(!vizitat[v[s][i]])
-            {
-                cout<<v[s][i]<<" ";
-
-                vizitat[v[s][i]] = 1;
-                q.push(v[s][i]);
+    queue <int> q;
+    q.push(nod);
+    vizitat[nod] = 1;
+    cout<< nod<<' ';
+    while(!q.empty()) {
+        for (unsigned int i = 0; i < v[nod].size(); i++) {
+            if (vizitat[v[nod][i]] == 0) {
+                cout << v[nod][i] << ' ';
+                q.push(v[nod][i]);
+                vizitat[v[nod][i]] = 1;
             }
         }
-
         q.pop();
-        s = q.front();
-
+        nod = q.front();
     }
 }
 int main()
